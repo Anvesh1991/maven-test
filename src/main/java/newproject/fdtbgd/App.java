899f749e8810a93913e3d1c6932f4c4c036a1e15
Anvesh1991 <anwesh.999@gmail.com>
2018-04-04 18:03:02 +0530

    src/

---
+++ src/main/java/newproject/fdtbgd/App.java
@@ -0,0 +1,13 @@
+package newproject.fdtbgd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
